af9ece39fc58baddd5076c67bba56bac0c75e85f
swordlegend32 <giopro1986@gmail.com>
2025-02-08 23:09:30 +0400

    Commit

--- Login/AccountClass.py
+++ Login/AccountClass.py
@@ -3,16 +3,16 @@
 class Account():
 
     # აქაუნთის შესაქმნელი ფუნქცია რომელიც გამოიძახება როცა Account() ფუნქციას გამოიყენებ
-
     def __init__(self):
         pass
 
+    # აქაუნთში შესვლა
     def LogIn(self,name:str,password:str,e_mail:str) -> str:
         pass
-
+    # აქაუნთიდან გამოსვლა
     def LogOut(self):
         self = None
-
+        
     def LoadData(self):
         pass
 
