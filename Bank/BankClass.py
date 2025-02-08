c4ec1516ceeb01a5a9784101ff9cde956549e70e
swordlegend32 <giopro1986@gmail.com>
2025-02-08 22:49:02 +0400

    Created Bank Class
    შევქმენი ბანკის კლასი

---
+++ Bank/BankClass.py
@@ -0,0 +1,16 @@
+
+
+class Bank():
+    # ბანკის შესაქმნელი ფუნქცია რომელიც გამოიძახება როცა Bank() ფუნქციას გამოიყენებ
+    def __init__(self):
+        pass
+
+    # self აღნიშნავს თავის თავს ანუ ბანკს
+
+    # Amount : float არის type check რომელიც გადააქცევს მოცემულ არგუმენტს იმ ტიპად რასაც აძლევ
+
+    def Deposit(self,Amount : float) -> bool:
+        pass
+
+    def Withdraw(self,Amount: float) -> bool:
+        pass
