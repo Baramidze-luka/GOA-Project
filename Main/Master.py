3cd617b2f4bb1e740f84724912e2c4f783c553a7
swordlegend32 <giopro1986@gmail.com>
2025-02-09 14:59:57 +0400

    Push

--- Main/Master.py
+++ Main/Master.py
@@ -1,11 +1,10 @@
-from Bank.BankClass import *
+from Bank.BankClass import Bank
 
 # დავაიმპორტედ ჩვენი ფაილი რომელიც დევს სხვა ფოლდერში
 
-Bank = Bank()
+UserBank = Bank()
 
 # Bank.Deposit(150.0)
 # Bank.Withdraw(100.0)
 # print(Bank.Balance)
-
 
